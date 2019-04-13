d7101e40664d698be483e9071b75d9d74cc73865
Arvianti <arviantiyulia16@gmail.com>
2019-04-13 21:39:41 +0700

    benerin dashboard

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get(
-    #     'DATABASE_URL') or 'postgresql://postgres:postgres@localhost/gosehat'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://andwjjsbvfhzxh:e88dcd61f880c6d6cd983d5118365fd23a745fd3b0b9c5e04567eb3af5519a14@ec2-184-73-169-151.compute-1.amazonaws.com/dbk8mnhi8mgcv8'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'postgresql://postgres:postgres@localhost/gosehat'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://andwjjsbvfhzxh:e88dcd61f880c6d6cd983d5118365fd23a745fd3b0b9c5e04567eb3af5519a14@ec2-184-73-169-151.compute-1.amazonaws.com/dbk8mnhi8mgcv8'
